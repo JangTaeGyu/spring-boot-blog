7be29286c615a0c85443030dcf690871c4c7e61d
JangTaeGyu <ttggbbgg2@gmail.com>
2023-04-30 00:02:11 +0900

    관리자 로그인 swagger 문서 추가

--- src/main/java/com/study/blog/account/presentation/LoginController.java
+++ src/main/java/com/study/blog/account/presentation/LoginController.java
@@ -4,6 +4,12 @@ import com.study.blog.account.application.query.LoginService;
 import com.study.blog.account.application.query.data.JwtTokenData;
 import com.study.blog.account.application.query.request.LoginRequest;
 import com.study.blog.account.presentation.response.JwtTokenResponse;
+import com.study.blog.springboot.exception.ValidationResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +29,18 @@ import javax.validation.Valid;
 public class LoginController {
     private final LoginService loginService;
 
+    @Operation(summary = "관리자 로그인")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK",
+                    content = @Content(schema = @Schema(implementation = JwtTokenResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+            @ApiResponse(
+                    responseCode = "422",
+                    description = "Unprocessable Entity",
+                    content = @Content(schema = @Schema(implementation = ValidationResponse.class)))
+    })
     @PostMapping
     public ResponseEntity<JwtTokenResponse> login(@RequestBody @Valid LoginRequest request) {
         log.info("[login] - {}", request);
