ae9dab9bbf71d7b4416e8527a31efd26ae3dac94
JangTaeGyu <ttggbbgg2@gmail.com>
2023-05-05 00:08:27 +0900

    회원 상세 정보 조회 api 추가

--- src/main/java/com/study/blog/account/presentation/UserController.java
+++ src/main/java/com/study/blog/account/presentation/UserController.java
@@ -3,6 +3,7 @@ package com.study.blog.account.presentation;
 import com.study.blog.account.application.command.UserCommandService;
 import com.study.blog.account.application.command.request.CreateUserRequest;
 import com.study.blog.account.application.command.request.UpdateUserRequest;
+import com.study.blog.account.application.query.UserService;
 import com.study.blog.account.domain.UserDto;
 import com.study.blog.account.presentation.response.UserResponse;
 import com.study.blog.springboot.constant.OpenApiConstant;
@@ -30,6 +31,7 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class UserController {
     private final UserCommandService userCommandService;
+    private final UserService userService;
 
     @Operation(
             summary = "회원 등록",
@@ -58,6 +60,18 @@ public class UserController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping("/{userCode}")
+    public ResponseEntity<UserResponse> show(@PathVariable String userCode) {
+        log.info("[show] - user code = {}", userCode);
+
+        UserDto user = userService.getUser(userCode);
+        log.info("[show] - successful");
+
+        UserResponse response = new UserResponse(user);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
     @Operation(
             summary = "회원 수정",
             security = @SecurityRequirement(name = OpenApiConstant.SECURITY_NAME))
@@ -79,7 +93,7 @@ public class UserController {
     public ResponseEntity<UserResponse> update(
             @PathVariable String userCode,
             @RequestBody @Valid UpdateUserRequest request) {
-        log.info("[update] - {}", request);
+        log.info("[update] - user code = {}, {}", userCode, request);
 
         UserDto user = userCommandService.updateUser(userCode, request);
         log.info("[update] - successful");
