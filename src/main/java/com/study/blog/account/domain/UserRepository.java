74e28877dd5cdfe8aee4864782a6836b8a69ab93
JangTaeGyu <ttggbbgg2@gmail.com>
2023-05-06 23:05:49 +0900

    회원 정보 수정 할 때 삭제된 회원은 수정이 안되도록 수정

--- src/main/java/com/study/blog/account/domain/UserRepository.java
+++ src/main/java/com/study/blog/account/domain/UserRepository.java
@@ -1,11 +1,16 @@
 package com.study.blog.account.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
 
 public interface UserRepository extends Repository<User, Long>, UserDao {
     Optional<User> findByEmail(String email);
+
+    @Query(
+            nativeQuery = true,
+            value = "select * from users where code = :code and deleted_at is null")
     Optional<User> findByCode(String code);
     boolean existsByCode(String code);
     boolean existsByEmail(String email);
